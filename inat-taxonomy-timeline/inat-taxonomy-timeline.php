a8a501042fb972449f6abd46075ac80016b2944b
tttalshaul <110480789+tttalshaul@users.noreply.github.com>
2023-09-07 01:21:19 +0300

    use ajax for timeline

--- inat-taxonomy-timeline/inat-taxonomy-timeline.php
+++ inat-taxonomy-timeline/inat-taxonomy-timeline.php
@@ -13,6 +13,11 @@ function inat_taxon_timeline_enqueue_scripts() {
     wp_enqueue_style('inat-taxonomy-timeline-css', plugin_dir_url(__FILE__) . 'style/inat-taxonomy-timeline.css', array(), '1.0');
 
     wp_enqueue_script('inat-taxonomy-timeline-js', plugin_dir_url(__FILE__) . 'js/inat-taxonomy-timeline.js', array('jquery'), '1.0', true);
+
+    // Pass data to JavaScript using wp_localize_script
+    wp_localize_script('inat-taxonomy-timeline-js', 'ajax_object', array(
+        'ajax_url' => admin_url('admin-ajax.php'), // WordPress AJAX endpoint
+    ));
 }
 add_action('wp_enqueue_scripts', 'inat_taxon_timeline_enqueue_scripts');
 
@@ -29,85 +34,40 @@ function inat_taxon_timeline_shortcode($atts) {
         return '<p>No taxon ID provided.</p>';
     }
 
-    // Define the color ranges
-    $color_ranges = array(
-        'white' => array('label' => 'No reports', 'min_percentage' => 0, 'max_percentage' => 0),
-        'lightgreen' => array('label' => 'Few reports', 'min_percentage' => 0, 'max_percentage' => 25),
-        'green' => array('label' => 'Average reports', 'min_percentage' => 25, 'max_percentage' => 75),
-        'darkgreen' => array('label' => 'Many reports', 'min_percentage' => 75, 'max_percentage' => 100),
-    );
-
-    $months = array(
-        1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 
-        7 => 0, 8 => 0, 9 => 0, 10 => 0, 11 => 0, 12 => 0,
-    );
-
-    $count_total = 0;
-
     // Generate the timeline HTML
-    $timeline_html = '<div class="inat-taxonomy-timeline">';
+    $timeline_html = '<div class="inat-taxonomy-timeline" data-taxon-id="' . esc_attr($taxon_id) . '"></div>';
 
-    // Make API request to iNaturalist
+    return $timeline_html;
+}
+
+// Define the AJAX callback function to retrieve iNaturalist data
+function get_inat_data() {
+    // Get the taxon ID from the AJAX request
+    $taxon_id = $_GET['taxon_id'];
+
+    // Make the API request to iNaturalist using your existing PHP code
     $api_url = 'https://api.inaturalist.org/v1/observations/histogram?place_id=6815&taxon_id=' . $taxon_id;
     $api_response = wp_remote_get($api_url);
 
     // Check if API request was successful
     if (!is_wp_error($api_response)) {
-        // Parse the API response
+        // Parse the API response and process the data
         $api_data = json_decode(wp_remote_retrieve_body($api_response), true);
-        $months = $api_data['results']['month_of_year'];
 
-        // Sum total
-        for ($month = 1; $month <= 12; $month++) {
-            $count_total += $months[$month];
-        }
+        // Extract the data you need from $api_data and format it as desired
+        $data = $api_data['results']['month_of_year']; // Replace with the data you want to return
 
-        // Loop through each month to build html
-        for ($month = 1; $month <= 12; $month++) {
-            $hebrew_month_name = get_month_name_hebrew($month);
-
-            // Calculate the percentage based on the maximum count
-            $percentage = ($months[$month] / $count_total) * 100;
-
-            // Determine the color based on the percentage
-            $color = '';
-            $label = '';
-            foreach ($color_ranges as $range_color => $range_data) {
-                if ($percentage >= $range_data['min_percentage'] && $percentage <= $range_data['max_percentage']) {
-                    $color = $range_color;
-                    $label = $range_data['label'];
-                    break;
-                }
-            }
-
-            $first_or_last = '';
-            if ($month == 1) {
-                $first_or_last = 'timeline-item-first';
-            }
-            else if ($month == 12) {
-                $first_or_last = 'timeline-item-last';
-            }
-
-            // Add the timeline item to the HTML
-            $timeline_html .= '<div class="timeline-item timeline-item-' . $color . ' ' . $first_or_last . '" data-month="' . $hebrew_month_name . '" data-count="' . $months[$month] . '">';
-            $timeline_html .= '<div class="timeline-label">' . $hebrew_month_name . '</div>';
-            $timeline_html .= '</div>';
-        }
+        wp_send_json_success($data);
+    } else {
+        // Handle errors if the API request fails
+        wp_send_json_error('Error fetching data from iNaturalist');
     }
 
-    $timeline_html .= '</div>';
-
-    return $timeline_html;
+    wp_die(); // Always include wp_die() at the end to terminate the AJAX request
 }
 
-// Helper function to get Hebrew month name
-function get_month_name_hebrew($month) {
-    $hebrew_month_names = array(
-        'ינואר', 'פברואר', 'מרץ', 'אפריל', 'מאי', 'יוני',
-        'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר', 'דצמבר'
-    );
-
-    return isset($hebrew_month_names[$month - 1]) ? $hebrew_month_names[$month - 1] : '';
-}
+// Hook the AJAX action to WordPress
+add_action('wp_ajax_get_inat_data', 'get_inat_data');
+add_action('wp_ajax_nopriv_get_inat_data', 'get_inat_data'); // For non-logged-in users
 
 ?>
--- inat-taxonomy-timeline/js/inat-taxonomy-timeline.js
+++ inat-taxonomy-timeline/js/inat-taxonomy-timeline.js
@@ -1,40 +1,125 @@
 jQuery(document).ready(function($) {
-    $('.timeline-item').click(function(e) {
-        var $this = $(this);
-        var month = $this.data('month');
-        var count = $this.data('count');
+    // Get the taxon ID from the data attribute of the timeline rectangle
+    var taxonID = $('.inat-taxonomy-timeline').data('taxon-id');
 
-        // Remove existing popups
-        $('.timeline-popup').remove();
+    // Make an AJAX request to fetch the data on page load
+    $.ajax({
+        type: 'GET',
+        url: ajax_object.ajax_url, // Use the WordPress AJAX endpoint
+        data: {
+            action: 'get_inat_data', // Create a custom AJAX action
+            taxon_id: taxonID,
+        },
+        success: function(response) {
+            // Process the response and format the HTML of the timeline
+            // You can format the HTML here based on the response data
 
-        // Create popup
-        var $popup = $('<div class="timeline-popup"></div>');
-        var $popupContent = $('<div class="popup-content"></div>');
-        var $popupMonth = $('<div class="popup-month">' + month + '</div>');
-        var $popupCount = $('<div class="popup-count">כמה נמצאו: ' + count + '</div>');
-        var $popupTip = $('<div class="popup-tip"></div>');
+            // Define the color ranges
+            var colorRanges = {
+                'white': { label: 'No reports', minPercentage: 0, maxPercentage: 0 },
+                'lightgreen': { label: 'Few reports', minPercentage: 0, maxPercentage: 25 },
+                'green': { label: 'Average reports', minPercentage: 25, maxPercentage: 75 },
+                'darkgreen': { label: 'Many reports', minPercentage: 75, maxPercentage: 100 }
+            };
 
-        // Append content to popup
-        $popupContent.append($popupMonth, $popupCount);
-        $popup.append($popupContent, $popupTip);
+            var months = response.data;
+            var countTotal = 0;
+            var timelineHTML = '';
 
-        // Append popup to rectangle
-        $this.append($popup);
+            // Sum total
+            for (var month = 1; month <= 12; month++) {
+                countTotal += months[month];
+            }
 
-        // Position the popup
-        var popupWidth = $popup.outerWidth();
-        var popupHeight = $popup.outerHeight();
-        var offsetX = popupWidth / 2;
-        var offsetY = popupHeight + 6;
-        $popup.css({
-            top: e.pageY - offsetY,
-            left: e.pageX - offsetX,
-        });
-    });
+            // Loop through each month to build HTML
+            for (var month = 1; month <= 12; month++) {
+                var hebrewMonthName = getMonthNameHebrew(month);
 
-    $(document).on('click', function(event) {
-        if (!$(event.target).closest('.timeline-item').length) {
-            $('.timeline-item').find('.timeline-popup').remove();
+                // Calculate the percentage based on the maximum count
+                var percentage = (months[month] / countTotal) * 100;
+
+                // Determine the color based on the percentage
+                var color = '';
+                var label = '';
+                for (var rangeColor in colorRanges) {
+                    if (
+                        percentage >= colorRanges[rangeColor].minPercentage &&
+                        percentage <= colorRanges[rangeColor].maxPercentage
+                    ) {
+                        color = rangeColor;
+                        label = colorRanges[rangeColor].label;
+                        break;
+                    }
+                }
+
+                var firstOrLast = '';
+                if (month === 1) {
+                    firstOrLast = 'timeline-item-first';
+                } else if (month === 12) {
+                    firstOrLast = 'timeline-item-last';
+                }
+
+                // Add the timeline item to the HTML
+                timelineHTML += '<div class="timeline-item timeline-item-' + color + ' ' + firstOrLast + '" data-month="' + hebrewMonthName + '" data-count="' + months[month] + '">';
+                timelineHTML += '<div class="timeline-label">' + hebrewMonthName + '</div>';
+                timelineHTML += '</div>';
+            }
+
+            // Update the content of the timeline rectangle with the formatted HTML
+            $('.inat-taxonomy-timeline').html(timelineHTML);
+
+            $('.timeline-item').click(function(e) {
+                var $this = $(this);
+                var month = $this.data('month');
+                var count = $this.data('count');
+
+                // Remove existing popups
+                $('.timeline-popup').remove();
+
+                // Create popup
+                var $popup = $('<div class="timeline-popup"></div>');
+                var $popupContent = $('<div class="popup-content"></div>');
+                var $popupMonth = $('<div class="popup-month">' + month + '</div>');
+                var $popupCount = $('<div class="popup-count">כמה נמצאו: ' + count + '</div>');
+                var $popupTip = $('<div class="popup-tip"></div>');
+
+                // Append content to popup
+                $popupContent.append($popupMonth, $popupCount);
+                $popup.append($popupContent, $popupTip);
+
+                // Append popup to rectangle
+                $this.append($popup);
+
+                // Position the popup
+                var popupWidth = $popup.outerWidth();
+                var popupHeight = $popup.outerHeight();
+                var offsetX = popupWidth / 2;
+                var offsetY = popupHeight + 6;
+                $popup.css({
+                    top: e.pageY - offsetY,
+                    left: e.pageX - offsetX,
+                });
+            });
+
+            $(document).on('click', function(event) {
+                if (!$(event.target).closest('.timeline-item').length) {
+                    $('.timeline-item').find('.timeline-popup').remove();
+                }
+            });
+        },
+        error: function() {
+            // Handle errors if the AJAX request fails
+            console.error('AJAX request failed');
         }
     });
 });
+
+// Helper function to get Hebrew month name
+function getMonthNameHebrew(month) {
+    var hebrewMonthNames = [
+        'ינואר', 'פברואר', 'מרץ', 'אפריל', 'מאי', 'יוני',
+        'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר', 'דצמבר'
+    ];
+
+    return hebrewMonthNames[month - 1] || '';
+}
