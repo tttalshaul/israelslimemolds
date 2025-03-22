b4d1decb9727e1789a4b48502442c7a9d9dd4e8a
tttalshaul <110480789+tttalshaul@users.noreply.github.com>
2025-03-22 23:47:22 +0200

    parse genbank file

---
+++ genbank_to_csv/genbank_to_csv.py
@@ -0,0 +1,28 @@
+from Bio import SeqIO
+import csv
+from datetime import datetime
+
+def format_date(d):
+    return datetime.strptime(d, '%d-%b-%Y').strftime('%m/%d/%Y')
+
+
+file_path = '/Users/user/Downloads/sequence.gb'
+genbank_object = SeqIO.parse(open(file_path, 'r'), "genbank")
+genes=list(genbank_object)
+
+with open('/Users/user/Downloads/myxo_genes.csv', 'w', newline='') as csvfile:
+    gene_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+    for gene in genes:
+        taxon = gene.annotations['organism']
+        date = format_date(gene.annotations['date'])
+        accession = gene.id
+        description = gene.description
+        references = gene.annotations['references']
+        full_reference = None
+        for reference in references:
+            if not full_reference or len(full_reference.title) < len(reference.title):
+                full_reference = reference
+        title = full_reference.title
+        authors = full_reference.authors
+        types = ", ".join([feature.type for feature in gene.features])
+        gene_writer.writerow([taxon, date, accession, description, title, authors, types])
